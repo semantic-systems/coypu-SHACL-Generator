926b3ff654242e364c297a3a9e1ff3b844ced366
Patrick Westphal <patrick.westphal@hitec-hamburg.de>
2024-01-09 11:16:16 +0100

    Add n-triples to TSV util function

--- util/__init__.py
+++ util/__init__.py
@@ -1,6 +1,17 @@
 from enum import Enum
 
+from rdflib import Graph
+
 
 class BackEnd(Enum):
     SHEXER = 'sheXer'
     SHACLGEN = 'Shaclgen'
+
+
+def nt_to_tsv(nt_input_file_path: str, tsv_output_file_path: str):
+    g = Graph()
+    g.parse(nt_input_file_path, format='ntriples')
+
+    with open(tsv_output_file_path, 'w') as out_file:
+        for s, p, o in g:
+            out_file.write(f'{str(s)}\t{str(p)}\t{str(o)}\n')
