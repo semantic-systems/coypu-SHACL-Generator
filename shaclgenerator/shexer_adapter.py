e4f5543f25922c500584127839f04628c817e54a
Patrick Westphal <patrick.westphal@hitec-hamburg.de>
2024-04-22 15:48:24 +0200

    Minor refactoring of gen._shacl and ShexerAdapter

--- shaclgenerator/shexer_adapter.py
+++ shaclgenerator/shexer_adapter.py
@@ -1,31 +1,36 @@
 from io import StringIO
-from typing import List
 
-from rdflib import Graph
-from shexer.shaper import Shaper
+from rdflib import Graph, RDF
 from shexer.consts import NT, SHACL_TURTLE
+from shexer.shaper import Shaper
 
 from shaclgenerator import SHACLGenerator
 
 
 class ShexerAdapter(SHACLGenerator):
-    def __init__(self, input_file_path: str, target_classes: List[str]):
-        self.shaper = Shaper(
-            target_classes=target_classes,
-            graph_file_input=input_file_path,
-            input_format=NT,
-        )
+    def __init__(self, input_file_path_or_sparql_endpoint: str):
+        if input_file_path_or_sparql_endpoint.startswith('http'):
+            self.shaper = Shaper(
+                all_classes_mode=True,
+                url_endpoint=input_file_path_or_sparql_endpoint,
+            )
+        else:
+            self.shaper = Shaper(
+                all_classes_mode=True,
+                graph_file_input=input_file_path_or_sparql_endpoint,
+                input_format=NT,
+            )
+        self.acceptance_threshold = 0.0
+        self.type_property = str(RDF.type)
             
-    def generate_shacl(self, acceptance_threshold) -> Graph:
+    def generate_shacl(self) -> Graph:
         g = Graph()
         g.parse(
             StringIO(self.shaper.shex_graph(
                 output_format=SHACL_TURTLE,
                 string_output=True,
-                acceptance_threshold=acceptance_threshold,
+                acceptance_threshold=self.acceptance_threshold,
             )),
             format='ttl')
 
         return g
-    
-    
