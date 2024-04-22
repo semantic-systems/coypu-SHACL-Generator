897b41b6c56f8ca9d72dd0a618b4c09f198bbb09
Patrick Westphal <patrick.westphal@hitec-hamburg.de>
2024-04-22 15:50:22 +0200

    Minor refactoring of util

--- util/__init__.py
+++ util/__init__.py
@@ -1,25 +1,16 @@
+import logging
 from enum import Enum
-from rdflib import Graph, URIRef
-from typing import List
 
 from rdflib import Graph
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('util')
 
 
 class BackEnd(Enum):
     SHEXER = 'sheXer'
     SHACLGEN = 'Shaclgen'
 
-def get_all_classes(
-        graph_file_input: str, 
-        type_property: str) -> List[str]:
-
-    g = Graph().parse(graph_file_input)
-
-    classes = set()
-    for s, p, o in g.triples((None, URIRef(type_property), None)):
-        classes.add(str(o))
-
-    return list(classes)
 
 def nt_to_tsv(nt_input_file_path: str, tsv_output_file_path: str):
     g = Graph()
